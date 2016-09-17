b56d7782c0c8d6ca7fe9ba7ca61fb746d45e8a46
spezi77 <spezi77@gmx.com>
2016-09-17 11:16:40 +0200

    Disable ADB authentication.
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- configs/system_additions.mk
+++ configs/system_additions.mk
@@ -29,3 +29,10 @@ PRODUCT_COPY_FILES += \
 # Security Enhanced Linux
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.selinux=1
+
+# Disable ADB authentication
+ADDITIONAL_DEFAULT_PROPERTIES += \
+    ro.adb.secure=0 \
+    ro.secure=0 \
+    persist.service.adb.enable=1
+
