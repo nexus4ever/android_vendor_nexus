f21ec6869a80c6fd24a060dda3dbf7e40c3d1cbe
spezi77 <spezi77@gmx.com>
2016-11-26 13:16:20 +0100

    Use pixel-mods and revert to white pixel bootanimation.
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- configs/nexus_phone.mk
+++ configs/nexus_phone.mk
@@ -17,6 +17,7 @@ include vendor/nexus/configs/bootanimation.mk
 include vendor/nexus/configs/nexus_main.mk
 include vendor/nexus/configs/system_additions.mk
 include vendor/nexus/configs/version.mk
+include vendor/nexus/pixel-mods/pixel_mods.mk
 
 # Telephony packages
 PRODUCT_PACKAGES += \
