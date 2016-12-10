ced20030b270b5b7bbe30aa1913bd88d66c7c1c9
spezi77 <spezi77@gmx.com>
2016-12-10 11:40:02 +0100

    Bump up to Android 7.1-r02. Let's be prepared for the next release, @nitin1438
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- configs/version.mk
+++ configs/version.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-BUILD_VERSION := 7.1-r01
+BUILD_VERSION := 7.1-r02
 
 NEXUS_VERSION := NeXus4ever_mako-$(BUILD_VERSION)-$(shell date +%Y%m%d)
 
