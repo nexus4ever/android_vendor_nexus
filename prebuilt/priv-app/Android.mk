c8b4dfdc931ef1f7d53c8377866db16512215db3
spezi77 <spezi77@gmx.com>
2016-12-28 22:33:04 +0100

    Add Gboard. * also add statements to allow Gboard & Gdialer to replace the stock apks
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- prebuilt/priv-app/Android.mk
+++ prebuilt/priv-app/Android.mk
@@ -1,5 +1,17 @@
 LOCAL_PATH := $(call my-dir)
 
+# Install Gboard
+include $(CLEAR_VARS)
+LOCAL_MODULE := Gboard
+LOCAL_SRC_FILES := Gboard/Gboard.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_PRIVILEGED_MODULE := false
+LOCAL_OVERRIDES_PACKAGES := LatinIME
+LOCAL_MODULE_OWNER := google
+include $(BUILD_PREBUILT)
+
 # Install GoogleDialer
 include $(CLEAR_VARS)
 LOCAL_MODULE := GoogleDialer
@@ -8,6 +20,8 @@ LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_PRIVILEGED_MODULE := false
+LOCAL_OVERRIDES_PACKAGES := Dialer
+LOCAL_MODULE_OWNER := google
 include $(BUILD_PREBUILT)
 
 # Install KernelAdiutor
